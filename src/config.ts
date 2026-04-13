80ae53f6e4317f9c103a72c8f85ea60b42c3695c
MERAND Julien <JM279506@intra.cea.fr>
2026-04-13 15:43:49 +0200

    Update April 13th

--- src/config.ts
+++ src/config.ts
@@ -10,7 +10,7 @@ export const siteConfig = {
     github: "https://github.com/JulienMerand",
   },
   aboutMe:
-    "As a 25-year-old PhD student at CEA-List and Paris-Saclay University, I specialize in the intersection of Vision-Based Policy Learning and Dexterous Manipulation. My research aims to bridge the gap between high-level AI reasoning and low-level robotic control to achieve human-like hand coordination. I combine a strong theoretical foundation with a hands-on commitment to shaping the future of intelligent robotics. Beyond my academic research, I am a big sports fan (runner, cyclist, black-belt judoka) and a guitarist.",
+    "As a 25-year-old PhD student at CEA-List and Paris-Saclay University, I specialize in the intersection of Artificial Intelligence and Robotics through dexterous manipulation of any objects using anthropomorphic robotic hands. My research aims to bridge the gap between high-level AI reasoning and low-level robotic control to achieve human-like grasping and manipulation. I combine a strong theoretical foundation with a hands-on commitment to shaping the future of intelligent robotics. Beyond my academic research, I am a big sports fan (runner, cyclist, black-belt judoka) and a guitarist.",
   skills: [
     "Python", 
     "C/C++", 
@@ -132,7 +132,7 @@ export const siteConfig = {
     {
       name: "3D Printed Robot Arm",
       description:
-        "Ungoing personal project inspired by BCN3D's Moveo robot. Developed various kinematics and control applications in Python and C++ on ROS2 Humble.",
+        "Ungoing personal project inspired by BCN3D's Moveo robot. Developed various applications in Python and C++ on ROS2 Humble.",
       link: "https://github.com/JulienMerand/robot_arm",
       skills: ["ROS2", "Python", "C/C++", "Arduino", "Computer Vision"],
     },
@@ -140,11 +140,11 @@ export const siteConfig = {
       name: "Mobile robot navigation and path planning in dynamic indoor environments.",
       description:
         "Proposed a dynamic obstacle avoidance solution for mobile robots, leveraging machine learning for velocity estimation and cost mapping.",
-      // link: "",
+      link: "https://github.com/JulienMerand/msc_robotics_gdp_2024",
       skills: ["ROS2", "Python", "C/C++", "Computer Vision", "Machine Learning", "Path Planning"],
     },
     {
-      name: "Analog Guitar Effects Pedal",
+      name: "Analog Guitar Effects Pedal: Clone of the Friedman BE-OD",
       description:
         "Designed and built an analog guitar effects pedal, gaining hands-on experience in analog electronics and understanding how electronic components manipulate audio signals.",
       skills: ["Analog Electronics", "Signal Processing", "PCB Soldering", "Prototyping"],
